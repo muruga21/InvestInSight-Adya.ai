27f01032a38a26114d61725a0cdf8be13d7f4f9b
muruga21 <murugaperumal21062004@gmail.com>
2024-05-07 15:03:36 +0530

    connected login with be

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,18 +2,42 @@ import React, { useState } from 'react'
 import './Login.css'
 import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
 
-  const [userName, setUserName] = useState('');
+  const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [displayLoading, setDisplayLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) =>{
     e.preventDefault();
-    console.log(userName, password);
     setDisplayLoading(true);
+    console.log(username, password);
+    console.log(process.env.REACT_APP_BACKEND_URL)
+    try{
+      const response = await axios.post(process.env.REACT_APP_BACKEND_URL+'/user/login', 
+      {
+        username, password
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      if(response.status === 200){
+        document.cookie = `token=${response.data.token}`;
+        navigate('/');
+      }
+    }
+    catch(err){
+      console.log(err);
+      setErrorMessage(err.response.data.message);
+      setDisplayLoading(false);
+    }
   }
 
   return (
@@ -40,6 +64,7 @@ const Login = () => {
             variant="outlined"  
             onChange={(e)=>{setPassword(e.target.value)}}
           />
+          <div className='text-[#FF0000]'>{errorMessage}</div>
         <button className="button" onClick={(e)=>{handleSubmit(e)}} >
           {
             (!displayLoading)?'Login':<CircularProgress style={{ height: 30, width: 30}}/>
