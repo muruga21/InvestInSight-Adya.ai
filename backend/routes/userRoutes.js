2390293e16d585e7843ac55e03ae1b3918f39bd7
muruga21 <murugaperumal21062004@gmail.com>
2024-05-08 15:50:27 +0530

    fixed auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { login,signup, getUser } = require('../controllers/userController');
-const {auth} = require('../middleware/auth')
+const {checkUser} = require('../middleware/auth')
 
 router.post('/login',login)
 router.post('/signup',signup)
-router.get('/getUser',auth,getUser)
+router.get('/getUser',checkUser,getUser)
 
 module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,38 +2,8 @@ import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { useEffect } from 'react';
-import axios from 'axios';
-import useDispatch from 'react-redux';
 
 function App() {
-
-  const dispatch = useDispatch();
-
-  const getUser = async() => {
-    try{
-      const token = document.cookie.split('=')[1];
-      const response = await axios.get(process.env.REACT_APP_BACKEND_URL+'/user/getUser', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer '+token,
-        }
-      });
-      if(response.data.error){
-        console.log(response.data.message)
-      }
-      else{
-        console.log(response.data.message)
-      }
-    }
-    catch(err){
-      console.log(err.message)
-    }
-  }
-
-  useEffect(()=>{
-    getUser();
-  },[])
 
   return (
     <>
